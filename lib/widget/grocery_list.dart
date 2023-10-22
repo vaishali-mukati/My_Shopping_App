9275cab40308fc142439a28829742081ed04c778
vaishali mukati <vaishalimukativasuki@gmail.com>
2023-10-22 23:17:23 +0530

    added dropdown items

--- lib/widget/grocery_list.dart
+++ lib/widget/grocery_list.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/widget/new_item.dart';
 
-class GroceryList extends StatelessWidget{
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
 
+  State<GroceryList> createState() {
+      return _GroceryListState();
+  }
+}
+class _GroceryListState extends State<GroceryList>{
+
+
+  void _addIcon(){
+     Navigator.of(context).push(MaterialPageRoute(builder: (ctx) => const NewItem()));
+  }
 
   @override
   Widget build(BuildContext context) {
        return Scaffold(
            appBar: AppBar(
              title:const Text('Your Grocery'),
+             actions: [
+               IconButton(
+                   onPressed: _addIcon,
+                   icon:const  Icon(Icons.add)),
+             ],
            ),
          body: ListView.builder(
            itemCount: groceryItems.length,
